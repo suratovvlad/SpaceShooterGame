85b149dc7ae77d539f93c1dd97288c403a57115e
Vladislav Suratov <suratovvlad@gmail.com>
2016-11-28 05:33:21 +0300

    Added parallax effect to background

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class BGScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeZ;
+
+    private Vector3 _startPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        transform.position = _startPosition + Vector3.forward * newPosition;
+    }
+}
