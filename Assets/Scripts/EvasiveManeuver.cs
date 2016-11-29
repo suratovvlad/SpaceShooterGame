cb66e69f35bf4e491cebdcc67b943689edc2e783
Vladislav Suratov <suratovvlad@gmail.com>
2016-11-29 04:50:34 +0300

    Added maneuvering to enemi ship. Enemy ship added as spawning hazard

---
+++ Assets/Scripts/EvasiveManeuver.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EvasiveManeuver : MonoBehaviour
+{
+    public float dodge;
+    public float smoothing;
+    public float tilt;
+    public Vector2 startWait;
+    public Vector2 maneuverTime;
+    public Vector2 maneuverWait;
+    public Boundary boundary;
+
+    private float _currentSpeed;
+    private float _targetManeuver;
+    private Rigidbody _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _currentSpeed = _rigidbody.velocity.z;
+        StartCoroutine(Evade());
+    }
+
+    IEnumerator Evade()
+    {
+        yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
+
+        while (true)
+        {
+            _targetManeuver = Random.Range(1, dodge) * -Mathf.Sign(transform.position.x);
+            yield return new WaitForSeconds(Random.Range(maneuverTime.x, maneuverTime.y));
+            _targetManeuver = 0;
+            yield return new WaitForSeconds(Random.Range(maneuverWait.x, maneuverWait.y));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float newManeuver = Mathf.MoveTowards
+        (
+            _rigidbody.velocity.x,
+            _targetManeuver,
+            Time.time * smoothing
+        );
+
+        _rigidbody.velocity = new Vector3(newManeuver, 0.0f, _currentSpeed);
+
+        _rigidbody.position = new Vector3
+        (
+            Mathf.Clamp(_rigidbody.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(_rigidbody.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        _rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, _rigidbody.velocity.x * -tilt);
+    }
+}
